1182b9c484ccc10c2fc005aa2dc1b69af1e34126
Allison <allisonsouza10261@gmail.com>
2026-09-16 09:42:14 -0300

    refactor : adjusting method getWordDetail to convert word to lowerCase

--- src/services/dictionaryApi.js
+++ src/services/dictionaryApi.js
@@ -5,8 +5,9 @@ export async function getAllLanguages() {
   return languages;
 }
 export async function getWordDetails(languageCode, word) {
+  const wordLowerCase = word.toLowerCase();
   const languages = await fetch(
-    `https://freedictionaryapi.com/api/v1/entries/${languageCode}/${word}`,
+    `https://freedictionaryapi.com/api/v1/entries/${languageCode}/${wordLowerCase}`,
   ).then((response) => response.json());
   return languages;
 }
